5256f49dfa855f65a2ba6d05b8a77d84c67b1f35
devonzachary <dzz>
2017-05-13 15:23:35 -0700

    enemey emitters, using animation drivers

--- src/game_objects/enemies.py
+++ src/game_objects/enemies.py
@@ -1,60 +1,42 @@
 from math import sin
 from client.beagle.beagle_api import api as bgl
+from .pulse_emitter import pulse_emitter
 
 class basic_enemy():
     view = bgl.assets.get("beagle-2d/coordsys/16:9")
     texture = bgl.assets.get("bullets/texture/basic_bullet")
+    driver_rate = 1.0 / 60.0
 
     def __init__(self, **kwargs):
-        self.x = kwargs['x']
-        self.y = kwargs['y']
-
-        self.halo_amt = 0.0
+        self.x = self.base_x = kwargs['x']
+        self.y = self.base_y = kwargs['y']
+        self.driver = bgl.curve_driver( curve = bgl.assets.get("enemy_paths/curve/" + kwargs["sequence_key"] ),
+                                          rate = basic_enemy.driver_rate )
 
     def tick(self):
-        self.x -= 0.1
-        if(self.x > -20):
-            return True
+        self.driver.tick()
+        if(self.driver.is_finished()):
+            return False
+
+        p = self.driver.value()
+        self.x = self.base_x + p[0]
+        self.y = self.base_y + p[1]
+
+        return True
 
     def get_shader_params(self ):
         return { 
             "texBuffer"            : basic_enemy.texture,
             "translation_local"    : [ 0, 0 ],
-            "scale_local"          : [ 0.1,0.1],
+            "scale_local"          : [ 0.25,0.25],
             "translation_world"    : [ self.x, self.y],
             "scale_world"          : [ 1, 1],
             "view"                 : basic_enemy.view,
-            "rotation_local"       : self.x * 0.8,
+            "rotation_local"       : 0.0,
             "filter_color"         : [ 1.0, 1.0, 1.0, 1.0],
             "uv_translate"         : [ 0,0 ] }
 
 
-class pulse_emitter():
-    def __init__(self, **kwargs):
-        self.t = 0
-        self.x = kwargs['x']
-        self.y = kwargs['y']
-        self.rate = kwargs['rate']
-        self.range = kwargs['range']
-        self.ramp_speed = kwargs['ramp_speed']
-        self.template = kwargs['template']
-        self.ramp = 0.0
-        
-
-    def emit(self):
-        self.template(self)
-
-    def tick(self):
-        self.t = self.t + self.rate
-        if(self.t > self.range[1]):
-            return False
-        if(self.t > self.range[0]):
-           self.ramp = self.ramp + self.ramp_speed
-           if self.ramp > 1.0:
-                self.ramp = 0.0
-                self.emit()
-
-        return True
 
   
 class enemies(bgl.simple_tick_manager):
@@ -67,17 +49,18 @@ class enemies(bgl.simple_tick_manager):
         self.enemies = self.create_tickable( bgl.purging_tick_manager() )
         self.emitters = self.create_tickable( bgl.purging_tick_manager() )
 
-        self.create_emitter ( pulse_emitter( x = 20, y = 0, 
+        self.create_emitter ( pulse_emitter( x = 0, y = 0, 
                                       rate = 1.0/60.0, 
-                                      range =(5.0,15), 
+                                      start = 0.0,
                                       ramp_speed = 0.05, 
-                                      template = (lambda emitter : self.create_enemy(emitter) ) ))
+                                      sequence_key = "up_down",
+                                      template = (lambda emitter : self.create_enemy(emitter, "slow_move_left") ) ))
 
     def create_emitter(self, emitter ):
         return self.emitters.create_tickable( emitter )
 
-    def create_enemy( self, emitter ):
-        return self.enemies.create_tickable( basic_enemy( x = emitter.x, y = emitter.y ) )
+    def create_enemy( self, emitter, sequence_key ):
+        return self.enemies.create_tickable( basic_enemy( x = emitter.x, y = emitter.y, sequence_key = sequence_key ) )
 
     def render( self ):
         for enemy in self.enemies.tickables:
---
+++ src/game_objects/pulse_emitter.py
@@ -0,0 +1,39 @@
+from client.beagle.beagle_api import api as bgl
+
+class pulse_emitter():
+    driver_rate = 1.0 / 60.0
+
+    def __init__(self, **kwargs):
+        self.t = 0
+        self.x = self.base_x = kwargs['x']
+        self.y = self.base_y = kwargs['y']
+        self.rate = kwargs['rate']
+        self.start = kwargs['start']
+        self.ramp_speed = kwargs['ramp_speed']
+        self.template = kwargs['template']
+        self.ramp = 0.0
+        self.driver = bgl.curve_driver( curve = bgl.assets.get("emitter_paths/curve/" + kwargs["sequence_key"]), rate = pulse_emitter.driver_rate )
+
+    def emit(self):
+        self.template(self)
+
+    def tick(self):
+        self.driver.tick()
+
+        self.t = self.t + self.rate
+
+        if(self.driver.is_finished()):
+            return False
+
+        if(self.t > self.start):
+            self.driver.tick()
+            p = self.driver.value()
+            self.x = self.base_x + p[0]
+            self.y = self.base_y + p[1]
+
+            self.ramp = self.ramp + self.ramp_speed
+            if self.ramp > 1.0:
+                self.ramp = 0.0
+                self.emit()
+
+        return True
